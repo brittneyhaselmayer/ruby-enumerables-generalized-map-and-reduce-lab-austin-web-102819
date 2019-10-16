c5552c8e5c325f2d2d4bb0a62ab907a2627870a8
Brittney Haselmayer <brittneyhaselmayer@yahoo.com>
2019-10-16 00:44:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,12 +9,8 @@ new
 end
 
 
-def reduce(array,start_value=0)
-new=0
-counter=0
-while counter<array.length
-new += (yield(array[counter]))
-
-counter += 1
+def reduce(array,start_value=nil)
+if start_value
+  
 end
 end
